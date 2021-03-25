833f9b140f330b233ea926c23a76a61e556f3f15
Alex Isk <alexisk@Alexs-MacBook-Air.local>
2021-03-25 17:54:40 +0300

    MilesModern

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate (int cost){
+      int miles;
+        miles = cost / 20;
+        return miles;
+    }
+}
+    // метод calculate, который:
+    //    Принимает на вход один параметр: cost типа int
+    //    Возвращает рассчитанное количество миль (тип - int)
