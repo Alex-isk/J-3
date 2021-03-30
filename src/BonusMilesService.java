5f0d00e446d29b55abf0a5f33bcaa91b57200d1a
Alex-isk <79845756+Alex-isk@users.noreply.github.com>
2021-03-30 20:03:00 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,7 @@
-public class BonusMilesService {
-    public int calculate(int cost){
-      int miles;
+public class BonusService {
+    public int calculate(int cost) {
+        int miles;
         miles = cost / 20;
         return miles;
     }
 }
-    // метод calculate, который:
-    //    Принимает на вход один параметр: cost типа int
-    //    Возвращает рассчитанное количество миль (тип - int)
